3240aca37ee6f69ab44b6fcbd68aae24e5c36dfe
Bat7 <bat7evers@gmail.com>
2013-12-03 13:31:17 +0200

    LoginController & CuratorController

---
+++ AOLHack/AOLHack.Site/Controllers/CuratorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AOLHack.Site.Controllers
+{
+    public class CuratorController : Controller
+    {
+        //
+        // GET: /Curator/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
